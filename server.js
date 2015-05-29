379417f84ae8b4e7fd1563ebcc6279e1d827e740
alibasli <mehmetalibasli02@gmail.com>
2015-05-30 01:11:56 +0300

    last version

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var pcap = require("pcap");
 
-
+app.use(express.static(__dirname + '/JS'));
+ 
 app.get('/', function(req, res){
   // index.html dosyası istemcilere gönderiliyor...
   res.sendFile(__dirname + '/index.html');
